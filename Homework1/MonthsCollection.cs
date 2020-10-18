4be5ef2174d3c48f48af46633eccf616504bf544
Дмитрий Пак <dquest1996@gmail.com>
2020-10-18 19:52:52 +0300

    add first homework

---
+++ Homework1/MonthsCollection.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework1
+{
+    using System.Collections;
+
+    public class MonthsCollection : IEnumerable
+    {
+        private readonly string[] _months =
+        {
+            "January", "February", "March",
+            "April", "May", "June",
+            "Jule", "August", "September",
+            "October", "November", "December"
+        };
+
+        /// <summary>
+        /// Индексатор.
+        /// </summary>
+        /// <param name="index">Индекс</param>
+        public string this[int index]
+        {
+            get => _months[index];
+            set => _months[index] = value;
+        }
+
+        /// <summary>
+        /// Перечислитель.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            return _months.GetEnumerator();
+        }
+    }
+}
